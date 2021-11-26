32cf820970b88b8c95d9e3bc9aef15c89c8b814a
Giovane Aires Gomes <giovaneag17@outlook.com>
2021-11-25 22:44:04 -0300

    Finish the course adding favorite context

--- new-starting-project/src/components/meetups/MeetupItem.js
+++ new-starting-project/src/components/meetups/MeetupItem.js
@@ -1,24 +1,43 @@
-import Card from '../ui/Card'
+import { useContext } from "react";
+import FavoritesContext from "../../store/favorites-context";
+
+import Card from "../ui/Card";
 import classes from "./MeetupItem.module.css";
 
 function MeetupItem(props) {
-  return (
-      <Card>
+  const favoritesCtx = useContext(FavoritesContext);
+  const itemIsFavorite = favoritesCtx.itemIsFavorite(props.id);
 
-    <li className={classes.item}>
-      <div className={classes.image}>
-        <img src={props.image} alt={props.title} />
-      </div>
-      <div className={classes.content}>
-        <h3>{props.title}</h3>
-        <address>{props.address}</address>
-        <p>{props.description}</p>
-      </div>
-      <div className={classes.actions}>
-        <button>To Favorites</button>
-      </div>
-    </li>
-      </Card>
+  function toggleFavoritesStatusHandler() {
+    if (itemIsFavorite) {
+      favoritesCtx.removeFavorite(props.id);
+    } else {
+      favoritesCtx.addFavorite({
+        id: props.id,
+        title: props.title,
+        address: props.address,
+        image: props.image,
+        description: props.description,
+      });
+    }
+  }
+
+  return (
+    <Card>
+      <li className={classes.item}>
+        <div className={classes.image}>
+          <img src={props.image} alt={props.title} />
+        </div>
+        <div className={classes.content}>
+          <h3>{props.title}</h3>
+          <address>{props.address}</address>
+          <p>{props.description}</p>
+        </div>
+        <div className={classes.actions}>
+          <button onClick={toggleFavoritesStatusHandler}>{itemIsFavorite ? 'Remove from Favorites' : 'To Favorites'}</button>
+        </div>
+      </li>
+    </Card>
   );
 }
 
