b1bf4f298072b67a582f3a2eb112564455495184
Giovane Aires Gomes <giovaneag17@outlook.com>
2021-11-25 21:53:20 -0300

    Developed Add new meetup page

--- new-starting-project/src/pages/NewMeetup.js
+++ new-starting-project/src/pages/NewMeetup.js
@@ -1,6 +1,29 @@
+import { useNavigate } from "react-router-dom";
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
+
 function NewMeetupPage() {
-    return <div>NewMeetup Page</div>;
+  const navigate = useNavigate();
+  function addMeetupHandler(meetupData) {
+    fetch(
+      "https://react-getting-started-5d080-default-rtdb.firebaseio.com/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    ).then(() => {
+      navigate("/", { replace: true });
+    });
   }
-  
-  export default NewMeetupPage;
-  
+
+  return (
+    <section>
+      <h1>Add New Meetup</h1>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </section>
+  );
+}
+
+export default NewMeetupPage;
